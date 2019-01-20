3ffffb0efaced2063abb12bbd107306f531505cf
lshhadeh <shhadeh.leen@yahoo.com>
2019-01-20 02:01:32 +0200

    update

--- Ingenera-client/src/app/app.component.ts
+++ Ingenera-client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from './translate.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,7 @@ import { TranslateService } from './translate.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  constructor() {
    
-      console.log(translate.data);
-
   }
 }
