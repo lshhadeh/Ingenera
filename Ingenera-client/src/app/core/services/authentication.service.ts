648b1aba67c7ef4736af4e9297ff2be9c3a0df36
Leen Shehadeh <LShehadeh@aspire-infotech.net>
2019-01-20 02:11:20 +0200

    Update core files and theme path

--- Ingenera-client/src/app/core/services/authentication.service.ts
+++ Ingenera-client/src/app/core/services/authentication.service.ts
@@ -1,30 +1,11 @@
-import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
-import { ActivatedRoute } from '@angular/router';
-import { User } from '../../shared/models';
+
 
 @Injectable()
 export class AuthenticationService {
-   // redirectUrl: string;
 
 
-    constructor(
-        // private router: Router,
-        // private route: ActivatedRoute
-    ) {
-      //  this.redirectUrl = this.route.snapshot.queryParams['redirectUrl'] || '/';
-    }
-
-    public getUser(): User {
-        const localUsr = JSON.parse(localStorage.getItem('currentUser')) as User;
-
-        return localUsr;
-    }
-    private setUser(usr: User, isRemember = false): void {
-        localStorage.setItem('currentUser', JSON.stringify(usr));      
-    }
-
+    constructor() {}
 
 
 }
